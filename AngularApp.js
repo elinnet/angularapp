d18635634d89070bfdb289e94aab6ffa05329b38
Elin <elinnet@gmail.com>
2015-09-29 22:58:59 +0100

    creation of initial files

---
+++ AngularApp.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>AngularJS Binding</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
+  </head>
+  <body>
+    <div ng-app>
+      <h1>Binding</h1>
+      <input type="test" ng-model="message">
+    </div>
+  </body>
+</html>
