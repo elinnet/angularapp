e771c27c1cbb82b439502ff23a967cf03ea91a71
Elin <elinnet@gmail.com>
2015-09-30 20:55:38 +0100

    add advanced directive functionalities

--- app/appbasicdirectives.js
+++ app/appbasicdirectives.js
@@ -2,18 +2,16 @@
 
   angular.module('functionalities', [])
   .directive("entering", function() {
-    return function(scope,element){
+    return function(scope,element,attrs){
       element.bind("mouseenter",function(){
-        console.log("Mouse has entered the div");
+        element.addClass(attrs.entering);
       });
     };
   })
   .directive("leaving", function(){
-    return function(scope,element){
+    return function(scope,element,attrs){
       element.bind("mouseleave",function(){
-        console.log("Mouse  has left the div");
+        element.removeClass(attrs.entering);
       });
     };
-
-
-  })
+  });
