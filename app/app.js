41b16683a6a1b23dd753bec85ef6881d519455f9
Elin <elinnet@gmail.com>
2015-09-30 07:48:57 +0100

    revision of angular controllers:

---
+++ app/app.js
@@ -0,0 +1,10 @@
+angular.module('app',[]);
+
+angular.module('app').controller('MainCtrl',function($scope){
+  var self = this;
+  self.message = 'hello';
+
+  self.changeMessage = function(message){
+    self.message = message;
+  }
+});
